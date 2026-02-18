e1ea3f7569198ec69df9e4059ef75c32844d8aa1
Tporterinbox <tporterinbox@gmail.com>
2026-02-18 00:43:16 -0500

    submission

--- sql-noir-1-vanishing-briefcase.sql
+++ sql-noir-1-vanishing-briefcase.sql
@@ -1,4 +1,39 @@
 -- Record your SQL detective process here!  Write down: 
+
   -- 1. The SQL queries you ran
+
+  SELECT * FROM suspects
+WHERE attire = 'trench coat';
+
+-- id    	name	        attire	         scar
+-- 3	Frankie Lombardi	trench coat	left cheek
+-- 183	Vincent Malone	trench coat	left cheek
+-- 237	Christopher Black	trench coat	right cheek
+-- ----------
+
+SELECT description FROM crime_scene;
+
+SELECT description FROM crime_scene WHERE location = 'Blue Note Lounge';
+-- description
+-- A briefcase containing sensitive documents vanished. 
+-- A witness reported a man in a trench coat with a scar on his left cheek fleeing the scene.
+
+-- -----------
+
+SELECT transcript FROM interviews wHERE suspect_id = '183';
+
+-- I wasn’t going to steal it, but I did. ----- Vincent Malone 
+
+-- -------------------------------------------------------------------
+
+
   -- 2. Any notes or insights as SQL comments
+
+
+  -- -------------------------------------------------------------------
+
   -- 3. Your final conclusion: who did it?
+-- Congratulations, detective! You have successfully identified Vincent Malone as the culprit.
+-- Congratulations, detective! You have successfully identified Vincent Malone as the culprit.
+
+--  Vincent Malone 
--- sql-noir-2-stolen-sound.sql
+++ sql-noir-2-stolen-sound.sql
@@ -1,4 +1,47 @@
 -- Record your SQL detective process here!  Write down: 
   -- 1. The SQL queries you ran
-  -- 2. Any notes or insights as SQL comments
+
+  SELECT location FROM crime_scene;
+
+SELECT id FROM crime_scene  WHERE location = 'West Hollywood Records'; 
+-- 65
+
+ SELECT location FROM crime_scene WHERE location = 'West Hollywood Records'; 
+
+ SELECT description FROM crime_scene WHERE location = 'West Hollywood Records'; 
+
+ SELECT type FROM crime_scene WHERE location = 'West Hollywood Records';
+
+ SELECT clue FROM  witnesses WHERE id = '55';
+--  I saw a woman in a red dress running down the stairs just after the incident.
+
+ SELECT id FROM witnesses WHERE crime_scene_id= '55';
+--  7 and 65
+
+SELECT clue FROM witnesses WHERE crime_scene_id= '65
+';
+
+SELECT clue FROM  witnesses WHERE crime_scene_id = '65';
+-- I saw a man wearing a red bandana rushing out of the store.
+-- The main thing I remember is that he had a distinctive gold watch on his wrist
+
+SELECT name FROM suspects WHERE bandana_color='red';
+
+
+SELECT name FROM suspects WHERE bandana_color='red' AND  accessory= 'gold watch';
+-- Tony Ramirez id35
+-- Mickey Riveran id44
+-- Rico Delgado  id97
+
+SELECT transcript  FROM interviews WHERE suspect_id= '97';
+-- I couldn't help it. I snapped and took the record.
+-- -Rico Delgado
+
+-- Excellent work, detective! Rico Delgado has confessed to stealing the prized vinyl record.
+
+--  ------------------------------------------
+
+  -- 2. Any notes or insights as SQL comment
+
+  -- -------------------------------------
   -- 3. Your final conclusion: who did it?
